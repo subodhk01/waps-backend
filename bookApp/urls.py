1b9474e107c08c29076f1f6eb6192ab5cc56bc2f
Rahulparmar01 <rahul220.iit@gmail.com>
2020-05-18 21:35:32 +0530

    urls and serializer for bookapp added

--- bookApp/urls.py
+++ bookApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+from .views import feedView
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('',include)
-]
+
+router=DefaultRouter()
+router.register('',feedView,)
+
+urlpatterns = router.urls
 
 
--- waps/urls.py
+++ waps/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    # path('books/',include('bookApp.urls')),
+    path('books/',include('bookApp.urls')),
 ]
