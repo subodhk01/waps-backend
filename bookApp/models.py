f0d36108f76e62fbdca0e6580fda95acc0d6ca90
Rahulparmar01 <rahul220.iit@gmail.com>
2020-05-19 13:51:48 +0530

    User model added and year problem solved

--- bookApp/models.py
+++ bookApp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .choices import borrowing_choice,YEAR_CHOICES
 import datetime
+
 
 # Create your models here.
 class Keyword(models.Model):
@@ -14,11 +16,7 @@ class Book(models.Model):
     owner=models.ForeignKey(User, on_delete=models.CASCADE,related_name='User', null=True)
     
     description=models.TextField(max_length=512,blank=True)
-    borrowing_choice=(
-        ('R','Rent'),
-        ('S','Sell'),
-        ('B','Both')
-    )
+    
     price=models.IntegerField(blank=False,default=0)
     #Borrowing method
     bMethod=models.CharField(max_length=1,choices=borrowing_choice)
@@ -26,10 +24,8 @@ class Book(models.Model):
     #status True means availabe
     borrowedBy=models.ForeignKey(User, on_delete=models.CASCADE,related_name='+',null=True)
     status=models.BooleanField(default=True)
-    YEAR_CHOICES = []
-    for r in range(1800, (datetime.datetime.now().year+1)):
-        YEAR_CHOICES.append((r,r))
-
+    
+    
     publication_year = models.IntegerField( choices=YEAR_CHOICES, default=datetime.datetime.now().year)
     cover=models.ImageField(blank=None, upload_to='./books/covers/', height_field=None, width_field=None, max_length=None)
     keywords=models.ManyToManyField(Keyword, related_name='keywords',null=True)
