532097b21f3d3505f99276766a0753d432b3ab39
Rahulparmar01 <rahul220.iit@gmail.com>
2020-05-19 13:58:43 +0530

    choices redefined

--- bookApp/models.py
+++ bookApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .choices import borrowing_choice,YEAR_CHOICES
+from .choices import BORROWING,YEARS
 import datetime
 
 
@@ -19,14 +19,14 @@ class Book(models.Model):
     
     price=models.IntegerField(blank=False,default=0)
     #Borrowing method
-    bMethod=models.CharField(max_length=1,choices=borrowing_choice)
+    bMethod=models.CharField(max_length=1,choices=BORROWING)
 
     #status True means availabe
     borrowedBy=models.ForeignKey(User, on_delete=models.CASCADE,related_name='+',null=True)
     status=models.BooleanField(default=True)
     
     
-    publication_year = models.IntegerField( choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    publication_year = models.IntegerField( choices=YEARS, default=datetime.datetime.now().year)
     cover=models.ImageField(blank=None, upload_to='./books/covers/', height_field=None, width_field=None, max_length=None)
     keywords=models.ManyToManyField(Keyword, related_name='keywords',null=True)
 
